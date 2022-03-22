9760ca857135134bd31069387ea384a008a38301
teresacabell <teresacabll@gmail.com>
2022-03-22 17:14:06 -0500

    fixed code for tests

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
-    </div>  
-    );
+    </div>
+  );
 }
 
 export default App;
